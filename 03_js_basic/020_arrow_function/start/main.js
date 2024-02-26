043004d1c0710fddbe9ea9b9439dff9a30bb3741
kaorukoyama <rufii72865@gmail.com>
2024-02-26 09:24:49 +0900

    callback関数の情報の渡り方の確認

--- 03_js_basic/020_arrow_function/start/main.js
+++ 03_js_basic/020_arrow_function/start/main.js
@@ -1,3 +1,13 @@
 function fn(number) {
   return number * 2;
 }
+
+console.log(fn(2));
+
+const fnArrow = (number) => {
+  console.log(number);
+  return number * 2;
+};
+
+const fnArrowObj = number => ({result: number * 2});
+console.log(fnArrowObj(2));
---
+++ 03_js_basic/030_esmodule/start/main.js
@@ -0,0 +1,7 @@
+import functionC,{ hello, User } from "./module.js";
+
+hello();
+functionC();
+
+const user = new User(`TOM`);
+user.hello();
